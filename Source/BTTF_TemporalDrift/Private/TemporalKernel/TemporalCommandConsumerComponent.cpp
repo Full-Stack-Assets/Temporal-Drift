5b7c0c8c4f10bc7cd944b58707f5ac401b85985d
Nicholas Albertson <nicholas.albertson@students.maestrocollege.edu>
2026-07-18 03:56:41 -0400

    feat: implement idempotent temporal command consumer

---
+++ Source/BTTF_TemporalDrift/Private/TemporalKernel/TemporalCommandConsumerComponent.cpp
@@ -0,0 +1,57 @@
+#include "TemporalKernel/TemporalCommandConsumerComponent.h"
+
+#include "TemporalKernel/TemporalWorldAdapterSubsystem.h"
+#include "Engine/World.h"
+
+UTemporalCommandConsumerComponent::UTemporalCommandConsumerComponent()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UTemporalCommandConsumerComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (UWorld* World = GetWorld())
+    {
+        if (UTemporalWorldAdapterSubsystem* Adapter = World->GetSubsystem<UTemporalWorldAdapterSubsystem>())
+        {
+            Adapter->RegisterConsumer(this);
+        }
+    }
+}
+
+void UTemporalCommandConsumerComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    if (UWorld* World = GetWorld())
+    {
+        if (UTemporalWorldAdapterSubsystem* Adapter = World->GetSubsystem<UTemporalWorldAdapterSubsystem>())
+        {
+            Adapter->UnregisterConsumer(this);
+        }
+    }
+
+    Super::EndPlay(EndPlayReason);
+}
+
+bool UTemporalCommandConsumerComponent::CanHandleCommand(const FSimulationCommandRecord& Command) const
+{
+    return !ConsumerId.IsNone() && SupportedCommandTypes.Contains(Command.CommandType);
+}
+
+bool UTemporalCommandConsumerComponent::ReceiveTemporalCommand(const FSimulationCommandRecord& Command)
+{
+    if (!CanHandleCommand(Command))
+    {
+        return false;
+    }
+
+    if (AppliedCommandIds.Contains(Command.CommandId))
+    {
+        return true;
+    }
+
+    AppliedCommandIds.Add(Command.CommandId);
+    OnCommandReceived.Broadcast(Command);
+    return true;
+}
