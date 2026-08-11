99a466c6b69a54e84e04959524f9d9e8735cde7d
Nic Albertson <nicholas.albertson@students.maestrocollege.edu>
2026-08-11 09:46:20 -0400

    test: cover approved RunGraph API surface

--- CausalCityPrototype/tests/kernel/public-api.test.js
+++ CausalCityPrototype/tests/kernel/public-api.test.js
@@ -6,6 +6,8 @@ import * as kernel from '../../src/kernel/index.js';
 test('Trust Kernel v1 exposes the approved additive API surface', () => {
   for (const name of [
     'createRun', 'advanceRun', 'forkRun', 'exportRun', 'replayRun', 'verifyRun',
+    'createRunGraph', 'forkBranch', 'getBranch', 'listChildren', 'listAncestors',
+    'exportRunGraph', 'parseRunGraph', 'verifyRunGraph',
     'recordAnomaly', 'appendAnomalyReview',
   ]) assert.equal(typeof kernel[name], 'function', name);
 });
