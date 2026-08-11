1226fa19541cee9766f3aba3ba53353557fbcea1
Nic Albertson <nicholas.albertson@students.maestrocollege.edu>
2026-08-11 09:24:49 -0400

    feat: export RunGraph v1 API

--- CausalCityPrototype/src/kernel/index.js
+++ CausalCityPrototype/src/kernel/index.js
@@ -1,6 +1,16 @@
 export { createRun, advanceRun, exportRun, replayRun } from './replay.js';
 export { verifyRun } from './verify.js';
 export { forkRun } from './branch.js';
+export {
+  createRunGraph,
+  forkBranch,
+  getBranch,
+  listChildren,
+  listAncestors,
+  exportRunGraph,
+  parseRunGraph,
+  verifyRunGraph,
+} from './run-graph.js';
 export { createAnomalyRegistry, recordAnomaly, appendAnomalyReview } from './anomalies.js';
 export { createManifest, manifestCore } from './manifest.js';
 export { createPrng, seedToState } from './prng.js';
